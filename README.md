60467f2a0badf4e69296c3f1daae3c75d341130e
Anish\ Gupta <mranish592@gmail.com>
2024-03-16 01:56:13 +0530

    feat: parsing options and output format

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ A cli tool written in python to count lines, words, characters and bytes
 1. Python alias added in ~/.zshrc
 2. How to take file input for python 
 3. How to read lines as text in python
-4. Hw to read files as binary in python
+4. How to read files as binary in python
+
+## Features pending
+1. should be called via an alias
+2. should be able to parse different options
+3. print in same format as wc
+4. Take input from stream
+5. take multiple files and concatenate the result
--- wc.py
+++ wc.py
@@ -2,57 +2,107 @@ import sys
 
 print("hello world from wc cli")
 
+def print_wc_output(counts, filepath):
+    spacing = "    "
+    for count in counts:
+        print(spacing + str(count), end="")
+        spacing = spacing[:-1]
+    print(" " + filepath)
+
 def parse_arguments(argv):
-    if(argv[1] != '-c'):
-        print("Incorrect usage. The syntax is: ")
-        print("python wc.py -c <filename>")
+    argv.pop(0)
+    print(argv)
+    file = ""
+    useDefaultOptions = True
+    options = {'c': False, 'm': False, 'l': False, 'w': False}
+    for arg in argv:
+        if (arg.startswith("-")):
+            useDefaultOptions = False
+            optionsToAdd = list(arg[1:])
+            print("optionsToAdd: ", optionsToAdd)
+            for option in optionsToAdd:
+                if(option == 'c'): options['m'] = False
+                elif (option == 'm'): options['c'] = False
+                elif (option != 'w' and option != 'l'):
+                    print("wc-cli: illegal option -- " + option)
+                    print("usage: wc [-clmw] [file ...] ")
+                    exit()
+                options[option] = True
+        else: 
+            file = arg
+            print("file: ", arg)
+    
+    if(useDefaultOptions):
+        options = {'c': True, 'm': False, 'l': True, 'w': True}
+
+    return [options, file]
+
+def read_binary(filepath, options):
+    try: 
+        file = open(filepath, "rb")
+
+        line_count = 0
+        byte_count = 0
+        word_count = 0
+
+        line = file.readline()
+        while(line):
+            if(options['l']): line_count = line_count + 1
+            if(options['c']): byte_count = byte_count + len(line)
+            if(options['w']): 
+                words = line.strip().split()
+                word_count = word_count + len(words)
+            line = file.readline()
+        
+        file.close()
+        counts = []
+        if(options['l']): counts.append(line_count)
+        if(options['w']): counts.append(word_count)
+        if(options['c']): counts.append(byte_count)
+        
+        print_wc_output(counts, filepath)
+
+    except Exception as e: 
+        print("wc-cli: " + filepath + ": " + e.args[1])
         exit()
-    return ['c', argv[2]]
 
-def read_binary(filepath):
-    file = open(filepath, "rb")
-    line = file.readline()
 
-    line_count = 0
-    byte_count = 0
-    word_count = 0
-    while(line):
-        line_count = line_count + 1
-        byte_count = byte_count + len(line)
-        words = line.strip().split()
-        word_count = word_count + len(words)
+def read_text(filepath, options):
+    try: 
+        file = open(filepath)
         line = file.readline()
-    
-    file.close()
-    print("lines: ", line_count)
-    print("bytes: ", byte_count)
-    print("words: ", word_count)
 
-def read_text(filepath):
-    file = open(filepath)
-    line = file.readline()
+        line_count = 0
+        char_count = 0
+        word_count = 0
+        while(line):
+            if(options['l']): line_count = line_count + 1
+            if(options['m']): char_count = char_count + len(line) + 1
+            if(options['w']): 
+                words = line.strip().split()
+                word_count = word_count + len(words)
+            line = file.readline()
+        
+        file.close()
+        counts = []
+        if(options['l']): counts.append(line_count)
+        if(options['w']): counts.append(word_count)
+        if(options['m']): counts.append(char_count)
+        
+        print_wc_output(counts, filepath)
 
-    line_count = 0
-    char_count = 0
-    word_count = 0
-    while(line):
-        line_count = line_count + 1
-        char_count = char_count + len(line) + 1
-        words = line.strip().split()
-        word_count = word_count + len(words)
-        line = file.readline()
-    
-    file.close()
-    print("lines: ", line_count)
-    print("characters: ", char_count)
-    print("words: ", word_count)
+    except Exception as e: 
+        print("wc-cli: " + filepath + ": " + e.args[1])
+        exit()
 
 print(sys.argv)
 [options, filepath] = parse_arguments(sys.argv)
 print("options: ", options)
 print("filename: ", filepath)
 
-read_binary(filepath)
-read_text(filepath)
+if(options['m']):
+    read_text(filepath, options)
+else:
+    read_binary(filepath, options)
 
 
