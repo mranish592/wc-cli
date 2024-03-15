1dec1350807ae9f75bedb519e2e4f461a80a58de
Anish\ Gupta <mranish592@gmail.com>
2024-03-15 20:28:10 +0530

    feat: counting logic for all 4 options

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wc-cli
 A cli tool written in python to count lines, words, characters and bytes
+
+## Development steps
+1. Python alias added in ~/.zshrc
+2. How to take file input for python 
+3. How to read lines as text in python
+4. Hw to read files as binary in python
--- wc.py
+++ wc.py
@@ -1,1 +1,58 @@
+import sys
+
 print("hello world from wc cli")
+
+def parse_arguments(argv):
+    if(argv[1] != '-c'):
+        print("Incorrect usage. The syntax is: ")
+        print("python wc.py -c <filename>")
+        exit()
+    return ['c', argv[2]]
+
+def read_binary(filepath):
+    file = open(filepath, "rb")
+    line = file.readline()
+
+    line_count = 0
+    byte_count = 0
+    word_count = 0
+    while(line):
+        line_count = line_count + 1
+        byte_count = byte_count + len(line)
+        words = line.strip().split()
+        word_count = word_count + len(words)
+        line = file.readline()
+    
+    file.close()
+    print("lines: ", line_count)
+    print("bytes: ", byte_count)
+    print("words: ", word_count)
+
+def read_text(filepath):
+    file = open(filepath)
+    line = file.readline()
+
+    line_count = 0
+    char_count = 0
+    word_count = 0
+    while(line):
+        line_count = line_count + 1
+        char_count = char_count + len(line) + 1
+        words = line.strip().split()
+        word_count = word_count + len(words)
+        line = file.readline()
+    
+    file.close()
+    print("lines: ", line_count)
+    print("characters: ", char_count)
+    print("words: ", word_count)
+
+print(sys.argv)
+[options, filepath] = parse_arguments(sys.argv)
+print("options: ", options)
+print("filename: ", filepath)
+
+read_binary(filepath)
+read_text(filepath)
+
+
