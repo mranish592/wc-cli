dc7231fbbb73883bf0a722b1c52610aa40cc4b28
Anish\ Gupta <mranish592@gmail.com>
2024-03-24 16:00:26 +0530

    fix: installtion script alias path

--- install-alias-bash.sh
+++ install-alias-bash.sh
@@ -3,7 +3,7 @@ wc_cli_alias_name=wc-cli
 workdir=$(pwd)
 
 if ! grep -q "alias $wc_cli_alias_name=" ~/.bashrc; then
-    echo "alias $wc_cli_alias_name='python3 ${workdir}/wc_cli.py'" >> ~/.bashrc
+    echo "alias $wc_cli_alias_name='python3 ${workdir}/wc-cli/wc_cli.py'" >> ~/.bashrc
     source ~/.bashrc
     echo "Alias '$wc_cli_alias_name' added successfully."
 else
--- install-alias-zsh.sh
+++ install-alias-zsh.sh
@@ -3,7 +3,7 @@ wc_cli_alias_name=wc-cli
 workdir=$(pwd)
 
 if ! grep -q "alias $wc_cli_alias_name=" ~/.zshrc; then
-    echo "alias $wc_cli_alias_name='python3 ${workdir}/wc_cli.py'" >> ~/.zshrc
+    echo "alias $wc_cli_alias_name='python3 ${workdir}/wc-cli/wc_cli.py'" >> ~/.zshrc
     source ~/.zshrc
     echo "Alias '$wc_cli_alias_name' added successfully."
 else
